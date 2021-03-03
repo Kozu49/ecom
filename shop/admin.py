f777a50dc5613dc7866664d70a3259362b9b7f63
manoj koju <manoj@offshorethemes.com>
2021-03-03 11:52:49 +0545

    admin customization

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from .models import Products, Order
 # Register your models here.
 
-admin.site.register(Products)
+
+class ProductAdmin(admin.ModelAdmin):
+    def change_category_to_defaul(self, request, queryset):
+        queryset.update(category="default")
+
+    change_category_to_defaul.short_description = "Default Category"
+
+    list_display = ("title", "price", "discount_price",
+                    "category")
+    search_fields = ("title",)
+    actions = ("change_category_to_defaul",)
+    # fields = ("title", "price",)
+    list_editable = ("price", "category", "discount_price")
+
+
+admin.site.register(Products, ProductAdmin)
 admin.site.register(Order)
